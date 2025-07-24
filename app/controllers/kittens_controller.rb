ae49fb87a4a01d14019df5697eabb41ad324f37e
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-24 15:59:18 -0400

    Completed kitten creation machine

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -14,7 +14,7 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.create(kitten_params)
     if @kitten.save
-      redirect_to @kitten, notice: "You've created a new kitten"
+      redirect_to @kitten, notice: "You've created a new kitten!"
     else
       render :new, status: :unprocessable_entity
     end
@@ -27,7 +27,7 @@ class KittensController < ApplicationController
   def update
     @kitten = Kitten.find(params[:id])
     if @kitten.update(kitten_params)
-      redirect_to @kitten, notice: "You've updated your kitten"
+      redirect_to @kitten, notice: "You've updated your kitten!"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -36,6 +36,8 @@ class KittensController < ApplicationController
   def destroy
     @kitten = Kitten.find(params[:id])
     @kitten.destroy
+    flash[:notice] = "Kitten has been eliminated! How could you do this!"
+    redirect_to root_path
   end
 
   private
