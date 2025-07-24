eebe0f3a0a654b9c41190e96c61074224efc8e60
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-23 23:34:09 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_23_182037) do
+  create_table "kittens", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.integer "cuteness"
+    t.integer "softness"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
