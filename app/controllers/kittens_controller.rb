b7aa1d0de7ca5608d9d351b3f782e25959f9e5fe
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-07-23 20:56:48 -0400

    Updated the remaining kitten controller actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,21 +4,37 @@ class KittensController < ApplicationController
   end
 
   def show
-    @kittens = Kittens.find(params[:id])
+    @kitten = Kittens.find(params[:id])
   end
 
   def new
+    @kitten = Kitten.new
   end
 
   def create
+    @kitten = Kitten.create(kitten_params)
+    if @kitten.save
+      redirect_to @kitten, notice: "You've created a new kitten"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @kitten = Kitten.find(params[:id])
   end
 
   def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update(kitten_params)
+      redirect_to @kitten, notice: "You've updated your kitten"
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
   end
 end
